def830da80da711b4fa443ec95dfde6afbdcdcf3
Deepanshu Rawat <rawat.deepanshu9968@gmail.com>
2026-05-11 19:57:18 +0530

    for error fixing

--- src/graphql/user/resolvers.ts
+++ src/graphql/user/resolvers.ts
@@ -1,5 +1,13 @@
+import type { CreateUserPayload } from "../../services/user.js";
+import UserService from "../../services/user.js";
+
 const queries = {};
 
-const mutations = {};
+const mutations = {
+  createUser: async (_: any, payload: CreateUserPayload) => {
+    const res = await UserService.createUser(payload);
+    return res.id;
+  },
+};
 
 export const resolvers = { queries, mutations };
